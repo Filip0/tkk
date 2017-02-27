3754c8da95fe540d894dafcb906ad6b493ea4254
Filip Egge <filip.egge@gmail.com>
2017-02-27 14:44:43 +0100

    Update welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @promoted_events = Event.limit(3)
+    @events = Event.upcoming
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ActiveRecord::Base
+
+  scope :upcoming, -> { where('start_date >= ?', Date.today)}
 end
