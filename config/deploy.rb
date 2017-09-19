c5589bf6e3f105c42835807408c40a1a5c6d5abb
Filip Egge <filip.egge@gmail.com>
2017-09-19 20:11:43 +0200

    Update linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,7 @@ set :rvm_ruby_version, '2.3.4'
 set :linked_files, %w{config/database.yml config/secrets.yml}
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
