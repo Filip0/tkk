d25e7c1bbd9cb19dff4c4d78b457142b3247f376
Filip Egge <filip.egge@gmail.com>
2018-03-18 14:25:11 +0100

    Clean up forms, add datetimepicker

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,14 +1,12 @@
-$(document).on("page:load page:change ready", function() {
-	console.log('PAGE LOAD');
+document.addEventListener("turbolinks:load", function() {
+	$( "#tabs" ).tabs({
+		collapsible: true
+	});
+
+	$('.datetimepicker').datetimepicker();
 	var elements = $(".simplemde");
 	for (var i = elements.length - 1; i >= 0; i--) {
 		console.log(elements[i]);
 		new SimpleMDE({ element: elements[i] });
 	};
-});
-
-$( function() {
-	$( "#tabs" ).tabs({
-		collapsible: true
-	});
-} );
+})
